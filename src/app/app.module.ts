b5f58526d82bfff479c4a55d7ed835559903ede0
Darwin Carbonel <dcarbonel1706@gmail.com>
2017-09-30 16:21:13 -0500

    Configuracion data Json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+//Router
 import  {app_routing} from './app.routes';
 
+//Services
+import {InformacionService} from './services/informacion.service';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -22,9 +25,10 @@ import { ProductoComponent } from './components/producto/producto.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     app_routing
   ],
-  providers: [],
+  providers: [InformacionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
