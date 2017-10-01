7576b8b87fd1b23e82fcd1ef1e78e35267c3f853
Darwin Carbonel <dcarbonel1706@gmail.com>
2017-10-01 01:31:15 -0500

    Carga de loading portafolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ const app_routes: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'producto', component: ProductoComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }//Para cualquier pagina no encontrada , enviarla al home
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes,{useHash:true});
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import { Http } from '@angular/http';
 @Injectable()
 export class ProductosService {
 
-  productos : any[]=[];
-  cargando:boolean=true;
-  constructor(public http:Http) {
+  productos: any[] = [];
+  cargando: boolean = true;
+  constructor(public http: Http) {
     this.cargar_productos();
   }
 
 
-  public cargar_productos(){
-    this.cargando=true;
-    if(this.productos.length===0){
+  public cargar_productos() {
+    this.cargando = true;
+    if (this.productos.length === 0) {
       this.http.get('https://paginaweb-d784a.firebaseio.com/productos_idx.json')
-      .subscribe(res => {
-         this.productos=res.json();
-        // console.log(res.json());
-        this.cargando=false;
-      });
+        .subscribe(res => {
+          setTimeout(
+            () => {
+              this.productos = res.json();
+              // console.log(res.json());
+              this.cargando = false;
+            },1500
+          )
+        });
     }
   }
 }
