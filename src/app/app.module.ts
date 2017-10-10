17ed8120c673f22b98ec3a5ed735cf10ae16f325
Darwin Carbonel <dcarbonel1706@gmail.com>
2017-10-09 22:40:17 -0500

    Pagina de busqueda creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProductoComponent } from './components/producto/producto.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ProductoComponent } from './components/producto/producto.component';
     FooterComponent,
     PortafolioComponent,
     AboutComponent,
-    ProductoComponent
+    ProductoComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import {
   AboutComponent,
   ProductoComponent,
-  PortafolioComponent
+  PortafolioComponent,
+  SearchComponent
 } from './components/index.paginas';
 
 const app_routes: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'producto/:id', component: ProductoComponent },
+  { path: 'buscar/:termino', component: SearchComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }//Para cualquier pagina no encontrada , enviarla al home
 ];
 
